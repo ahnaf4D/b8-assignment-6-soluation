42a0290d8d4ee8cfe3cee2e96c2f518c0d4d2c8b
ahnaf4D <ahnaftahmid802@gmail.com>
2024-03-01 12:58:52 +0600

    some more funcitonality added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const btnContainer = document.getElementById('btn-container');
 const cardContainer = document.getElementById('card-container');
 let selectedCatagory = 1001;
+const errorElement = document.getElementById(`error-element`);
+// console.log(errorElement);
 const fetchCatagories = () => {
     const url = `https://openapi.programming-hero.com/api/videos/categories`;
     fetch(url)
@@ -24,27 +26,43 @@ const fetchDataCatagories = (categoryId) => {
         .then(res => res.json())
         .then(({ data }) => {
             cardContainer.textContent = ' ';
+            if(data.length == 0){
+                errorElement.classList.remove('hidden');
+            }
+            else{
+                errorElement.classList.add('hidden');
+            }
             data.forEach((video) => {
-                // console.log(video);
+                console.log(video);
+                let verifiedBadge = ' '
+                if(video.authors[0].verified){
+                    verifiedBadge =  `<img class="w-6 h-6" src="./images/verify.png" alt="">`
+                }
+                else{
+                    verifiedBadge =  `<img class="w-6 h-6" src="./images/non-verified.jpg" alt="">`
+                }
                 const newCard = document.createElement('div');
                 newCard.innerHTML = `
                 <div class="card w-full bg-base-100 shadow-xl">
                 <figure class="overflow-hidden h-72">
-                    <img class="w-full" src="./images/smells.jpg" alt="Shoes" />
+                    <img class="w-full" src="${video.
+                        thumbnail
+                        }" alt="Shoes" />
                     <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
                 </figure>
                 <div class="card-body">
                     <div class="flex space-x-4 justify-start items-start">
                         <div>
-                            <img class="w-12 h-12 rounded-full" src="./images/smells.jpg" alt="Shoes" />
+                            <img class="w-12 h-12 rounded-full" src="${video.authors[0].profile_picture}" alt="Shoes" />
                         </div>
                         <div>
-                            <h2 class="card-title">Laugh At My Pain</h2>
+                            <h2 class="card-title">${video.title}</h2>
                             <div class="flex mt-3">
-                                <p class="">Author Name</p>
-                                <img class="w-6 h-6" src="./images/verify.png" alt="">
+                                <p class="">${video.authors[0].
+                                    profile_name}</p>
+                               ${verifiedBadge}
                             </div>
-                            <p class="mt-3">100k Views</p>
+                            <p class="mt-3">${video.others.views} Views</p>
                         </div>
                     </div>
                 </div>
