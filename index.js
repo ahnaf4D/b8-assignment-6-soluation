2075bb02f0a4853ea30aab97b833d11c87957ab4
ahnaf4D <ahnaftahmid802@gmail.com>
2024-03-01 12:37:43 +0600

    Dynamic card created

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const btnContainer = document.getElementById('btn-container');
+const cardContainer = document.getElementById('card-container');
+let selectedCatagory = 1001;
 const fetchCatagories = () => {
     const url = `https://openapi.programming-hero.com/api/videos/categories`;
     fetch(url)
@@ -17,6 +19,41 @@ const fetchCatagories = () => {
 }
 const fetchDataCatagories = (categoryId) => {
     console.log(categoryId);
+    const url = `https://openapi.programming-hero.com/api/videos/category/${categoryId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(({ data }) => {
+            cardContainer.textContent = ' ';
+            data.forEach((video) => {
+                // console.log(video);
+                const newCard = document.createElement('div');
+                newCard.innerHTML = `
+                <div class="card w-full bg-base-100 shadow-xl">
+                <figure class="overflow-hidden h-72">
+                    <img class="w-full" src="./images/smells.jpg" alt="Shoes" />
+                    <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
+                </figure>
+                <div class="card-body">
+                    <div class="flex space-x-4 justify-start items-start">
+                        <div>
+                            <img class="w-12 h-12 rounded-full" src="./images/smells.jpg" alt="Shoes" />
+                        </div>
+                        <div>
+                            <h2 class="card-title">Laugh At My Pain</h2>
+                            <div class="flex mt-3">
+                                <p class="">Author Name</p>
+                                <img class="w-6 h-6" src="./images/verify.png" alt="">
+                            </div>
+                            <p class="mt-3">100k Views</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                `
+                cardContainer.appendChild(newCard);
+            })
+        });
 }
-fetchDataCatagories();
+// fetchDataCatagories();
 fetchCatagories();
+fetchDataCatagories(selectedCatagory)
